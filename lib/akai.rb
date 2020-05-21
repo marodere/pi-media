f7bf8e4b331bdd886ef23b365370f2f0bc510427
marodere <marodere@up4k.org>
2020-05-21 19:13:47 +0300

    event handling: akai_bind

--- lib/akai.rb
+++ lib/akai.rb
@@ -19,7 +19,22 @@ __akai_note_pads = (36..43)
 # initialization
 #
 
-use_real_time
+__registered_events = Hash.new()
+
+define :__process_event do |channel_no, event_type, key, value=nil|
+  if !__registered_events.has_key? channel_no then
+    return
+  end
+  if !__registered_events[channel_no].has_key? event_type then
+    return
+  end
+  if !__registered_events[channel_no][event_type].has_key? key then
+    return
+  end
+  __registered_events[channel_no][event_type][key].each do |event|
+    event.call value
+  end
+end
 
 if get[:__akai_initialized] == nil then
   __akai_channels.each do |channel_no|
@@ -45,33 +60,41 @@ __akai_channels.each do |channel_no|
   key_prefix = "__akai_state_ch" + channel_no.to_s
   
   in_thread(name: th_prefix + "_program_change") do
+    use_real_time
     loop do
       x = sync __akai_device + ch + "/program_change"
       set (key_prefix + "_program"), x[0]
+      __process_event ch, :program, x[0]
     end
   end
   
   in_thread(name: th_prefix + "_control_change") do
+    use_real_time
     loop do
       cc_no, value = sync __akai_device + ch + "/control_change"
       set (key_prefix + "_cc"),
         get[key_prefix + "_cc"].merge({cc_no => value})
+      __process_event ch, :control, cc_no, value
     end
   end
   
   in_thread(name: th_prefix + "_note_on") do
+    use_real_time
     loop do
       note_no, value = sync __akai_device + ch + "/note_on"
       set (key_prefix + "_note"),
         get[key_prefix + "_note"].merge({note_no => value})
+      __process_event ch, :note, note_no, value
     end
   end
   
   in_thread(name: th_prefix + "_note_off") do
+    use_real_time
     loop do
       note_no, _ = sync __akai_device + ch + "/note_off"
       set (key_prefix + "_note"),
         get[key_prefix + "_note"].merge({note_no => nil})
+      __process_event ch, :note, note_no
     end
   end
 end
@@ -153,4 +176,53 @@ define :akai_note_pressed? do |note_no, channel_no=nil|
   return (akai_note note_no, channel_no) != nil
 end
 
-# events TBD
+# events
+
+define :akai_bind do |event_type, key, callback, channel_no=nil|
+  ch = __akai_channel channel_no
+  assert ([:program, :control, :handle, :note].include? event_type), __akai_log_prefix + "ERROR: no such event type!"
+  # TBD: validate key
+  # TBD: validate callback duration (should be == 0)
+  
+  if event_type == :handle then
+    event_type = :control
+    key = key + __akai_cc_handles.first - 1
+  end
+  
+  if !__registered_events.has_key? ch then
+    __registered_events[ch] = Hash.new
+  end
+  if !__registered_events[ch].has_key? event_type then
+    __registered_events[ch][event_type] = Hash.new
+  end
+  if !__registered_events[ch][event_type].has_key? key then
+    __registered_events[ch][event_type][key] = Array.new
+  end
+  __registered_events[ch][event_type][key].push(callback)
+end
+
+#  #
+#  # example: control equalizer + lpf & hpf
+#  #
+#
+#  define :norm do |value|
+#    return (value - 64) / 64.0
+#  end
+#
+#  with_fx :eq do |eq|
+#    akai_bind :handle, 1, lambda {|v| control eq, low: (norm v)}
+#    akai_bind :handle, 2, lambda {|v| control eq, mid: (norm v)}
+#    akai_bind :handle, 3, lambda {|v| control eq, high: (norm v)}
+#    with_fx :lpf, cutoff: 130 do |lpf|
+#      with_fx :hpf, cutoff: 0 do |hpf|
+#        akai_bind :handle, 4, lambda {|v|
+#            control lpf, cutoff: [v * 2 + 2, 130].min
+#            control hpf, cutoff: [(v - 64) * 2, 0].max
+#        }
+#
+#        # play something
+#
+#      end
+#    end
+#  end
+#
